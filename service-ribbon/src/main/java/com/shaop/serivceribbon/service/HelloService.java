b01ea280fa7f50f3362e7ca5fa5667053d91b80e
ShaoPing <258lsp@sina.com>
2019-12-04 13:04:37 +0800

    消息总线

--- service-ribbon/src/main/java/com/shaop/serivceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/shaop/serivceribbon/service/HelloService.java
@@ -1,6 +1,7 @@
 package com.shaop.serivceribbon.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,7 +15,11 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
     public String hiService(String name) {
+        //loadBalancerClient.choose("SERVICE-HI");
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
     }
 
